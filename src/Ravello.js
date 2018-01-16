eabbe7ae53eb33906b9c21262ba304a009e26b75
Codi Fiman <c.fiman@f5.com>
2018-01-16 23:37:53 +0000

    Provide configure method to mix provided configuration into existing config; provide config object to request method
    References: UDF-1056

--- src/Ravello.js
+++ src/Ravello.js
@@ -4,12 +4,21 @@ const methods = require('./methods');
 const request = require('./request');
 const waiters = require('./waiters');
 
-const composeMethod = ({ method, path }) => (body) => request({ path, method, body });
+// Set default configuration and provide a method for overriding
+const conf = {
+  Promise: Promise,
+};
+
+const configure = (newConf) => Object.assign(conf, newConf);
+
+// Compose library methods
+const composeMethod = ({ method, path }) => (body) => request({ path, method, body, conf });
 
 const builtMethods = {};
 
 Object.keys(methods).forEach((key) => { builtMethods[key] = composeMethod(methods[key]); });
 
-const Ravello = Object.assign({}, builtMethods, waiters);
+// Compose export
+const Ravello = Object.assign({ configure }, builtMethods, waiters);
 
 module.exports = Ravello;
