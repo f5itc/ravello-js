4151dd6f0f27bc57c50a1ff7d5be7d452f67c57c
Sky Fernandez <s.fernandez@f5.com>
2018-01-17 20:30:25 +0000

    Don't step on conf changes in Ravello
    Sorry Codi :O

--- src/Ravello.js
+++ src/Ravello.js
@@ -4,13 +4,15 @@ const methods = require('./methods');
 const request = require('./request');
 const waiters = require('./waiters');
 const constants = require('./constants');
+const configure = require('./conf').configure;
 
+// Compose library methods
 const composeMethod = ({ method, path }) => (body) => request({ path, method, body });
 
 const builtMethods = {};
 
 Object.keys(methods).forEach((key) => { builtMethods[key] = composeMethod(methods[key]); });
 
-const Ravello = Object.assign({}, builtMethods, waiters, constants);
+const Ravello = Object.assign({ configure }, builtMethods, waiters, constants);
 
 module.exports = Ravello;
